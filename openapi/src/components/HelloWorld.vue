8fbdc79762e61e1203a8e5aede85e5f2896aadbc
timothyd93 <timothydeng93@gmail.com>
2023-04-20 13:58:33 -0400

    a

--- openapi/src/components/HelloWorld.vue
+++ openapi/src/components/HelloWorld.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from "vue"
+import {ref, onMounted} from 'vue'
 const data = ref('')
 async function getData() {
   let res = await fetch('https://data.cityofnewyork.us/resource/qk7d-gecv.json')
   let data2 = await res.json()
-  data.value.values = data2.results 
+  data.value = data2.results 
 }
 onMounted(() => {
   getData()
