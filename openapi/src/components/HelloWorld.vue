22aa9ff03b59d73195016e13f557458e12dcc74f
timothyd93 <timothydeng93@gmail.com>
2023-04-26 14:00:17 -0400

    ada

--- openapi/src/components/HelloWorld.vue
+++ openapi/src/components/HelloWorld.vue
@@ -1,39 +1,38 @@
-
-
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
 </template>
 
 <script>
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-async function getData() {
-const response = await fetch("https://data.cityofnewyork.us/resource/qk7d-gecv.json")
-const data = await response.json()
-console.log(data)
-}
-getData()
-
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-  name: "BarChart",
+  name: 'BarChart',
   components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const data = await fetch("https://data.cityofnewyork.us/resource/qk7d-gecv.json")
+      this.chartdata = data
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
   }
 }
 </script>
+async function getData() {
+  const response = await fetch("https://data.cityofnewyork.us/resource/qk7d-gecv.json")
+  const data = await response.json()
+  console.log(data)
+  }
